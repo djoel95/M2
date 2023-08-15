21a8fe57f4d45c9908426bdfe1f11cd98fde2610
Joel Lopez <djoel95@hotmail.com>
2023-08-14 21:15:41 -0400

    REDUXX

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,22 +3,31 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+const store = createStore(contador);
 
-// Obtenemos el elemento con el id `valor`.
-var valor;
+// Obtenemos el elemento con el id 'valor'
+const valor = document.getElementById('valor');
 
-// Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
-// En el primer render y cada vez que nos subscribamos al Store.
-// Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
-  // Obtenemos la propiedad 'contador' de nuestro store:
-  // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+  // Obtenemos la propiedad 'contador' de nuestro store
+  const contador = store.getState().contador;
+
+  // Seteamos el número obtenido como texto dentro del elemento con id 'valor'
+  valor.textContent = contador;
 }
 
-// Ejecutamos la función 'renderContador':
+// Ejecutamos la función 'renderContador'
+renderContador();
 
-// Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
+// Nos suscribimos al store pasándole la función 'renderContador'
+store.subscribe(renderContador);
 
-// Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
-// hagan un dispatch al store de la acción correspondiente:
+// Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click
+// hagan un dispatch al store de la acción correspondiente
+document.getElementById('incremento').addEventListener('click', function() {
+  store.dispatch(incremento());
+});
+
+document.getElementById('decremento').addEventListener('click', function() {
+  store.dispatch(decremento());
+});
--- 11-Redux/homework/01 - Exercises/src/reducer/index.js
+++ 11-Redux/homework/01 - Exercises/src/reducer/index.js
@@ -9,7 +9,20 @@ const initialState = {
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
-  
+  switch (action.type) {
+    case INCREMENTO:
+      return {
+        ...state,
+        contador: state.contador + 1
+      };
+    case DECREMENTO:
+      return {
+        ...state,
+        contador: state.contador - 1
+      };
+    default:
+      return state;
+  }
 }
 
 module.exports = contador;
